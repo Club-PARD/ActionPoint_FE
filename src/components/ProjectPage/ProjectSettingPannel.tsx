13d07bd012a88f22648f79c6da546301ab89b597
Park Hasol <hasol1229@handong.ac.kr>
2025-07-08 16:07:43 +0900

    [FEAT]ProjectSettingPannel

--- src/components/ProjectPage/ProjectSettingPannel.tsx
+++ src/components/ProjectPage/ProjectSettingPannel.tsx
@@ -1,6 +1,4 @@
 'use client';
-
-import Image from 'next/image';
 import CopyCodeButton from './CopyCodeButton';
 import LeaveProjectButton from './LeaveProjectButton';
 import styles from '../../styles/ProjectSettingPannel.module.css';
@@ -12,16 +10,10 @@ interface Props {
 export default function ProjectSettingsPanel({ onClose }: Props) {
   return (
     <div className={styles.panel}>
-      <div className={styles.header}>
-        <h2 className={styles.title}>프로젝트 설정</h2>
-        <button onClick={onClose} className={styles.closeButton}>
-          <Image src="/XButton.svg" alt="leave icon" width={24} height={24} />
-        </button>
-      </div>
       <div className={styles.content}>
         <CopyCodeButton />
         <LeaveProjectButton />
       </div>
-    </div>
+    </div> 
   );
 }
