1f54c89d3f3402f402e0b8081761710cb10ac94b
Park Hasol <hasol1229@handong.ac.kr>
2025-07-09 15:28:50 +0900

    [FEAT]Header

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import styles from "../styles/LandingPage.module.css";
 import LoginButton from "../components/LandingPage/LoginButton";
 import ScrollToTopButton from "@/components/LandingPage/ScrollToTopButton";
-
-
+import Header from "@/components/Header/Header";
 
 export default function LandingPage() {
   return (
     <div className={styles.wrapper}>
+      <Header /> {/* ✅ 헤더 넣어주기 */}
+
       <img src="/landing1.png" className={styles.image} alt="랜딩1" />
 
       <div className={styles.buttonArea}>
@@ -18,8 +19,8 @@ export default function LandingPage() {
 
       <div className={styles.buttonArea}>
         <LoginButton />
-         
       </div>
+
       <ScrollToTopButton />
     </div>
   );
