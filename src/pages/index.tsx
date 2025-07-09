e3136bcecbda5f695d9fb9fe10ba2a75eb359229
Park Hasol <hasol1229@handong.ac.kr>
2025-07-09 16:25:47 +0900

    [FEAT]MainPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
-import LandingPage from "./LandingPage";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-export default function Home() {
-  return <LandingPage />;
+export default function Index() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/LandingPage"); // ✅ "/LandingPage"로 자동 이동
+  }, [router]);
+
+  return null; // 아무것도 안 보여줌
 }
